33a6448367f8979471d478cc45e53a9e2c601696
haniey <h.rastifar@gmail.com>
2022-05-07 10:11:44 -0700

    feat:adding sidebar and navbar components in adminlayout components

---
+++ src/layouts/adminLayout/components/Navbar.js
@@ -0,0 +1,14 @@
+import { AppBar, Toolbar } from '@mui/material';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <AppBar position='static'>
+            <Toolbar>
+
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Navbar;
