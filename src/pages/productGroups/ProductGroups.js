912b09b490c8ce351967a327720ae4e34b945b3b
haniey <h.rastifar@gmail.com>
2022-05-07 10:37:28 -0700

    feat:adding not purchagefinalizing  page

---
+++ src/pages/productGroups/ProductGroups.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductGroups = () => {
+    return (
+        <div>
+            <h1>This is product Group page</h1>
+        </div>
+    );
+};
+
+export default ProductGroups;
---
+++ src/pages/purchaseFinalizing.js/PurchaseFinalizing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PurchaseFinalizing = () => {
+    return (
+        <div>
+            <h1>This is purchage Finalizing</h1>
+        </div>
+    );
+};
+
+export default PurchaseFinalizing;
