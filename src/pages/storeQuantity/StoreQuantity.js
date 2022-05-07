d63bb6d060cbf49814428d60113afef255d4018f
haniey <h.rastifar@gmail.com>
2022-05-07 10:39:16 -0700

    feat:adding not storeQuantity page

---
+++ src/pages/storeQuantity/StoreQuantity.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StoreQuantity = () => {
+    return (
+        <div>
+             <h1>this is StoreQuantity</h1>
+        </div>
+    );
+};
+
+export default StoreQuantity;
