c4482864d2d249745235e3f1cc8230bc8a4d8850
haniey <h.rastifar@gmail.com>
2022-05-07 10:20:23 -0700

    feat:adding about us page and its component in pages folder

---
+++ src/pages/aboutus/Aboutus.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Aboutus = () => {
+    return (
+        <div >
+            <h1 >this is about us</h1>
+            
+
+        </div>
+    );
+};
+
+export default Aboutus;
