5081b4e3671340f40462ebde4fd52711033f989e
haniey <h.rastifar@gmail.com>
2022-05-07 10:56:50 -0700

    feat:wrapping index.js with material themeProvider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./styles/Styles"
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<ThemeProvider theme={theme}><App /></ThemeProvider>);
