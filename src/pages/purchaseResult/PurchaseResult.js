122f29e1ab479a911469b33806fab86e9c4bb8ed
haniey <h.rastifar@gmail.com>
2022-05-07 10:38:21 -0700

    feat:adding not purchageResult  page

---
+++ src/pages/purchaseResult/PurchaseResult.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PurchageResult = () => {
+    return (
+        <div>
+            <h1>This is purchage result</h1>
+        </div>
+    );
+};
+
+export default PurchageResult;
