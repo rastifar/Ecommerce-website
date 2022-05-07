c5f9962f347130bd4ca27f567d4a5a50744747b6
haniey <h.rastifar@gmail.com>
2022-05-07 10:34:31 -0700

    feat:adding not found page

---
+++ src/pages/pagenotfound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+           <h1 >صفحه مورد نظر یافت نشد</h1>
+        </div>
+    );
+};
+
+export default NotFound;
