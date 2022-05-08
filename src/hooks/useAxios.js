541e3433b576ba15ae03afb4b22f78f7c94e4f90
haniey <h.rastifar@gmail.com>
2022-05-07 20:20:45 -0700

    feat:adding some code to useAxios in hooks folder

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
 const useAxios = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const [response, setResponse] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [controller, setController] = useState();
+
+  const axiosFetch = async (configObj) => {
+      const { axiosInstance, method, url, requestConfig = {} } = configObj;
+      const ctrl = new AbortController();
+      setController(ctrl)
+      try {
+        setLoading(true)
+      const res = await axiosInstance[method.toLowerCase()](url, {
+        ...requestConfig,
+        signal: ctrl.singal,
+      });
+        console.log(res.data);
+        setResponse(res.data)
+    } catch (error) {
+        console.log(error);
+        setError(error.message)
+    } finally {
+        setLoading(false)
+    }
+  };
+//     useEffect(() => {
+//         console.log(controller);
+//         return (()=>controller.abort())
+// },[controller])
+  return [response,error,loading,axiosFetch];
 };
 
 export default useAxios;
