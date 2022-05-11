d663727492f0354515ea8c0ffaee37f35591728b
haniey <h.rastifar@gmail.com>
2022-05-11 01:54:32 -0700

    feat:adding store to redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import { configureStore } from "@reduxjs/toolkit";
 
-const store = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
 
-export default store;
+export const store = configureStore({
+    devTools: true,
+    preloadedState:loadPreloadState(),
+    reducer: {
+        user: userReducer,
+        invoice 
+    }
+})
+
+
+const loadPreloadState = () => {
+    try {
+        const serializedState = localStorage.getItem('state');
+        if (serializedState === null) {
+            return undefined;
+        }
+        return JSON.parse(serializedState)
+    } catch (error) {
+        return undefined;
+    }
+}
+const saveState = state => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('state',serializedState)
+    } catch {
+        
+    }
+}
+
+store.subscribe(() =>{ saveState({user: store.getState().user})})
