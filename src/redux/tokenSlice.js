e310a99275af8a0dbfb6e283d5e284757577e46a
haniey <h.rastifar@gmail.com>
2022-05-16 01:28:12 -0700

    feat:adding removetoken action to tokenSlice

--- src/redux/tokenSlice.js
+++ src/redux/tokenSlice.js
@@ -1,18 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {token:""};
+const initialState = { token: "" };
 
 export const tokenSlice = createSlice({
-    name: 'tokenSlice',
-    initialState,
-    reducers: {
-        addToken: (state,action) => {
-            return action.payload
-        }
-        
-    }
-})
+  name: "tokenSlice",
+  initialState,
+  reducers: {
+    addToken: (state, action) => {
+      return action.payload;
+    },
+      removeToken: (state) => {
+        state.token="",
+    },
+  },
+});
 
 export const { addToken } = tokenSlice.actions;
 
-export default tokenSlice.reducer
+export default tokenSlice.reducer;
