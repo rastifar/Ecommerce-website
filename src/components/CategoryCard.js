58921d7b684ea5da400fcab2c607816f5a93ebed
haniey <h.rastifar@gmail.com>
2022-05-16 20:44:43 -0700

    feat:completing categorycard

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
-const CategoryCard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default CategoryCard;
+export default function CategoryCard({title,image,alt}) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="200"
+          image={image}
+          alt={alt}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          </CardContent>
+      </CardActionArea>     
+    </Card>
+  );
+}
