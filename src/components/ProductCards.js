d3a7ba32d71f3b47e033f785770c60eb36d1c2df
haniey <h.rastifar@gmail.com>
2022-05-17 02:27:01 -0700

    feat:making the basic of productcards features

--- src/components/ProductCards.js
+++ src/components/ProductCards.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import * as React from "react";
+//material
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@mui/material";
 
-const ProductCards = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+//components
+import MyLink from "./MyLink";
 
-export default ProductCards;
+//---------------------------------------------
+
+export default function ProductCards({ image, name ,price}) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt={name}
+        height="200"
+        image={image}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        {price}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">افزودن به سبد خرید</Button>
+        {/* <Button size="small">Learn More</Button> */}
+      </CardActions>
+    </Card>
+  );
+}
