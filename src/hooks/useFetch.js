50dd6d6b81284930af0953776b2f99463d92eb3f
haniey <h.rastifar@gmail.com>
2022-05-17 23:51:00 -0700

    fix:changing the way using axios hook by sending the url to it

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,37 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "../api/httpRequestApi";
+import { toast } from 'react-toastify';
 
-const useAxios = () => {
+const useAxios = (url,requestConfig=[]) => {
   const [response, setResponse] = useState([]);
+  const [headers, setHeaders] = useState([]);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   // const [controller, setController] = useState();
 
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(url, {
+        ...requestConfig,
+      })
+      .then((res) => {
+        setResponse(res.data);
+        setHeaders(res.headers);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+        if (error.response?.status === 401) {
+          toast.error("شما دسترسی لازم را ندارید");
+        } else {
+          toast.error("خطایی روی داده است");
+        }
+      });
+  }, []);
+
   const axiosFetch = async (configObj) => {
-    const {url, requestConfig={} } = configObj;
+    const { url, requestConfig = {} } = configObj;
     // const ctrl = new AbortController();
     // setController(ctrl)
     try {
-      setLoading(true)
+      setLoading(true);
       console.log(requestConfig);
       const res = await axios.get(url, {
         ...requestConfig,
         // signal: ctrl.singal,
       });
       console.log(res.data);
-      setResponse(res.data)
+      setResponse(res.data);
     } catch (error) {
       console.log(error);
-      setError(error.message)
+      setError(error.message);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   };
   //     useEffect(() => {
   //         console.log(controller);
   //         return (()=>controller.abort())
   // },[controller])
-  return {products:response, error, loading, axiosFetch};
+  return { products: response, error, loading, axiosFetch };
 };
 
 export default useAxios;
