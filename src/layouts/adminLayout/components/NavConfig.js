63e1086670f49b1a0d4fccb440fc58af62660bd4
haniey <h.rastifar@gmail.com>
2022-05-23 11:31:29 -0700

    fix:commenting all the codes in 3 files related to admin layout

--- src/layouts/adminLayout/components/NavConfig.js
+++ src/layouts/adminLayout/components/NavConfig.js
@@ -1,36 +1,36 @@
-// component
-import Iconify from "../../../components/Iconify ";
+// // component
+// import Iconify from "../../../components/Iconify ";
 
-// ----------------------------------------------------------------------
+// // ----------------------------------------------------------------------
 
-const getIcon = (name) => <Iconify icon={name} width={22} height={22} />;
+// const getIcon = (name) => <Iconify icon={name} width={22} height={22} />;
 
-const navConfig = [
-  {
-    title: "dashboard",
-    path: "/dashboard",
-    icon: getIcon("eva:lock-fill"),
-  },
-  {
-    title: "goods",
-    path: "/dashboard/goods",
-    icon: getIcon("eva:pie-chart-2-fill"),
-  },
-  {
-    title: "orders",
-    path: "/dashboard/orders",
-    icon: getIcon("eva:people-fill"),
-  },
-  {
-    title: "storequantity",
-    path: "/dashboard/storequantity",
-    icon: getIcon("eva:file-text-fill"),
-  },
-  {
-    title: "Not found",
-    path: "/404",
-    icon: getIcon("eva:alert-triangle-fill"),
-  },
-];
+// const navConfig = [
+//   {
+//     title: "dashboard",
+//     path: "/dashboard",
+//     icon: getIcon("eva:lock-fill"),
+//   },
+//   {
+//     title: "goods",
+//     path: "/dashboard/goods",
+//     icon: getIcon("eva:pie-chart-2-fill"),
+//   },
+//   {
+//     title: "orders",
+//     path: "/dashboard/orders",
+//     icon: getIcon("eva:people-fill"),
+//   },
+//   {
+//     title: "storequantity",
+//     path: "/dashboard/storequantity",
+//     icon: getIcon("eva:file-text-fill"),
+//   },
+//   {
+//     title: "Not found",
+//     path: "/404",
+//     icon: getIcon("eva:alert-triangle-fill"),
+//   },
+// ];
 
-export default navConfig;
+// export default navConfig;
--- src/layouts/adminLayout/components/Navbar.js
+++ src/layouts/adminLayout/components/Navbar.js
@@ -1,23 +1,23 @@
-import { AppBar, Toolbar,Button } from "@mui/material";
-import React from "react";
-import { useNavigate } from "react-router-dom";
+// import { AppBar, Toolbar,Button } from "@mui/material";
+// import React from "react";
+// import { useNavigate } from "react-router-dom";
 
 
-const Navbar = () => {
-  const navigate = useNavigate();
-  const handleLogOut = () => {
-    window.localStorage.removeItem("token");
-    navigate("/", { replace: true });
-  };
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <Button color="inherit" onClick={handleLogOut}>
-          خروج
-        </Button>
-      </Toolbar>
-    </AppBar>
-  );
-};
+// const Navbar = () => {
+//   const navigate = useNavigate();
+//   const handleLogOut = () => {
+//     window.localStorage.removeItem("token");
+//     navigate("/", { replace: true });
+//   };
+//   return (
+//     <AppBar position="static">
+//       <Toolbar>
+//         <Button color="inherit" onClick={handleLogOut}>
+//           خروج
+//         </Button>
+//       </Toolbar>
+//     </AppBar>
+//   );
+// };
 
-export default Navbar;
+// export default Navbar;
