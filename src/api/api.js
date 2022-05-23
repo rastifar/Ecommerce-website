9d37ba4097e1babb1bb6f3201d07a7d1d42fcc0d
haniey <h.rastifar@gmail.com>
2022-05-23 04:32:51 -0700

    feat:adding the 3th radio btn to orders page

--- src/api/api.js
+++ src/api/api.js
@@ -6,46 +6,46 @@ import { toast } from "react-toastify";
 
 class Api {
   
-    constructor() {
-      axios.defaults.baseURL = BASE_URL;
-      // axios.defaults.headers.common['Authorization'] = localStorage.getItem("token");
-        axios.defaults.timeout = 2000;
-        axios.interceptors.request.use(
-          (config) => {
-            if(config.url===ORDERS){
-              config.headers["token"] =localStorage.getItem("token")
-            }
+  constructor() {
+    axios.defaults.baseURL = BASE_URL;
+    // axios.defaults.headers.common['Authorization'] = localStorage.getItem("token");
+    axios.defaults.timeout = 2000;
+    //   axios.interceptors.request.use(
+    //     (config) => {
+    //       // if(config.url==='orders'){
+    //       //   config.headers["token"] =localStorage.getItem("token")
+    //       // }
            
-            return config;
-          },
-          (error) => {
-            return Promise.reject(error);
-          }
-        );
+    //       return config;
+    //     },
+    //     (error) => {
+    //       return Promise.reject(error);
+    //     }
+    //   );
     
-        axios.interceptors.response.use(
-          (response) => {
-                if (response.statusCode === 401) {
+    //   axios.interceptors.response.use(
+    //     (response) => {
+    //           if (response.statusCode === 401) {
                 
-                    toast.error('شما دسترسی لازم برای این کار را ندارید', {
-                        position: "top-center",
-                        autoClose: 5000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: true,
-                        draggable: true,
-                        progress: undefined,
-                        })
-            }
+    //               toast.error('شما دسترسی لازم برای این کار را ندارید', {
+    //                   position: "top-center",
+    //                   autoClose: 5000,
+    //                   hideProgressBar: false,
+    //                   closeOnClick: true,
+    //                   pauseOnHover: true,
+    //                   draggable: true,
+    //                   progress: undefined,
+    //                   })
+    //       }
              
-            return response;
-          },
-          (error) => {
-            return Promise.reject(error);
-          }
-        );
-      }
-    
+    //       return response;
+    //     },
+    //     (error) => {
+    //       return Promise.reject(error);
+    //     }
+    //   );
+    // }
+  }
       get = async(url) => {
         try {
           const response = await axios.get(url);
--- src/pages/orders/Orders.js
+++ src/pages/orders/Orders.js
@@ -4,7 +4,7 @@ import useFetch from "../../hooks/useFetch";
 import { BASE_URL, ORDERS } from "../../constants/apiConst";
 
 //material
-import { DataGrid, faIR } from "@mui/x-data-grid";
+import { DataGrid, faIR, getDataGridUtilityClass } from "@mui/x-data-grid";
 import {
   Grid,
   Radio,
@@ -21,6 +21,8 @@ import { Link } from "react-router-dom";
 //reduxStore
 import { useSelector } from "react-redux";
 import axios from "axios";
+import OrderModal from "./components/OrderModal";
+import api from "../../api/api";
 //---------------------------------------------------------
 //columns
 const columns = [
@@ -62,13 +64,29 @@ const columns = [
 export default function Orders() {
   //const token = useSelector((state) => state.token);
   //const token = localStorage.getItem('token')
-  const token = useSelector((state) => state.token);
-  const [products,setProducts] = useState([])
+ const token = useSelector((state) => state.token);
+  const [isOpen, setIsOpen] = useState(false);
+  const [data, setData] = useState("");
+  const [products, setProducts] = useState([]);
   const [pageSize, setPageSize] = useState(5);
 
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+
+  // const getData = async () => {
+  //   setProducts(
+  //     await api.get("http://localhost:3002/orders", {
+  //       headers: { token: token },
+  //     })
+  //   );
+  // };
+
   useEffect(() => {
-    axios.get('http://localhost:3002/orders',{headers:{token:token}}).then(res=>setProducts(res.data))
-  },[])
+    axios
+      .get("http://localhost:3002/orders", { headers: { token: token } })
+      .then((res) => setProducts(res.data));
+  }, []);
   // const { products, error, loading } = useFetch(ORDERS, {
   //   headers: {
   //     token: token,
@@ -110,6 +128,7 @@ export default function Orders() {
       width: 300,
       sortable: false,
       editable: false,
+
       renderCell: (params) => (
         <a href="#" onClick={() => handleOrder(params)} sx={{ color: "blue" }}>
           بررسی سفارش
@@ -131,78 +150,94 @@ export default function Orders() {
   }));
 
   const handleOrder = (params) => {
-    console.log(params.row.purchaseTotal);
-  };
+    const id = params.row.id;
+    console.log(id);
 
+    console.log(products);
+    //await axios.get(BASE_URL + ORDERS + `/${id}`, { headers: { token: token } });
+    //axios.get(BASE_URL+ORDERS+`/${params.row.id}`,{headers:{token:token}}).then(res=>console.log(res.data))
+    //setData(params.row.id)
+    setData(products.find((item) => item.id === id));
+    setIsOpen(true);
+    console.log(data);
+  };
+  console.log(products);
   const handlechange = (value) => {
     if (value === 0) {
-      axios.get(BASE_URL + ORDERS,{headers:{token:token}}).then(res => setProducts(res.data))
-      return
+      axios
+        .get(BASE_URL + ORDERS, { headers: { token: token } })
+        .then((res) => setProducts(res.data));
+      return;
     }
-    axios.get(BASE_URL+ORDERS+`?orderStatus=${value}`).then(res=>setProducts(res.data))
-   
+    axios
+      .get(BASE_URL + ORDERS + `?orderStatus=${value}`)
+      .then((res) => setProducts(res.data));
+
     // products = useFetch(, {
     //   headers: {
     //     token: token,
     //   },
     // });
-  }
+  };
 
   return (
-    <Grid
-      container
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ p: 5 }}
-    >
-      <Grid container item sx={{ p: 2, background: "white", width: "100%" }}>
-        <Grid item xs={2} align="right" sx={{ flexGrow: 1 }}>
-          <Typography>مدیریت سفارش ها</Typography>
+    <>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ p: 5 }}
+      >
+        <Grid container item sx={{ p: 2, background: "white", width: "100%" }}>
+          <Grid item xs={2} align="right" sx={{ flexGrow: 1 }}>
+            <Typography>مدیریت سفارش ها</Typography>
+          </Grid>
+          <Grid item xs={10} align="center">
+            <RadioGroup
+              row
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="0"
+              name="radio-buttons-group"
+            >
+              <FormControlLabel
+                value="0"
+                control={<Radio />}
+                label="تمام سفارشات  
+"
+                onChange={() => handlechange(0)}
+              />
+              <FormControlLabel
+                value="1"
+                control={<Radio />}
+                label="سفارش های تحویل شده  
+"
+                onChange={() => handlechange(1)}
+              />
+              <FormControlLabel
+                value="2"
+                control={<Radio />}
+                label="سفارش های در انتظار ارسال"
+                onChange={() => handlechange(2)}
+              />
+            </RadioGroup>
+          </Grid>
         </Grid>
-        <Grid item xs={10} align="center">
-          <RadioGroup
-            row
-            aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue="female"
-            name="radio-buttons-group"
-          >
-            <FormControlLabel
-              value="0"
-              control={<Radio />}
-              label="تمام سفارشات  
-"
-              onChange={() => handlechange(0)}
-            />
-            <FormControlLabel
-              value="1"
-              control={<Radio />}
-              label="سفارش های تحویل شده  
-"
-              onChange={() => handlechange(1)}
-            />
-            <FormControlLabel
-              value="2"
-              control={<Radio />}
-              label="سفارش های در انتظار ارسال"
-              onChange={() => handlechange(2)}
-            />
-          </RadioGroup>
+        <Grid item sx={{ height: 400, width: "100%" }}>
+          <DataGrid
+            item
+            sx={{ background: "white" }}
+            rows={rows}
+            columns={columns}
+            pageSize={pageSize}
+            onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+            rowsPerPageOptions={[5, 10, 20]}
+            pagination
+            localeText={faIR.components.MuiDataGrid.defaultProps.localeText}
+          />
         </Grid>
       </Grid>
-      <Grid item sx={{ height: 400, width: "100%" }}>
-        <DataGrid
-          item
-          sx={{ background: "white" }}
-          rows={rows}
-          columns={columns}
-          pageSize={pageSize}
-          onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-          rowsPerPageOptions={[5, 10, 20]}
-          pagination
-          localeText={faIR.components.MuiDataGrid.defaultProps.localeText}
-        />
-      </Grid>
-    </Grid>
+      <OrderModal open={isOpen} onClose={() => setIsOpen(false)} data={data} />
+    </>
   );
 }
