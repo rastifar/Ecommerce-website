9e30a0703acec590d473b5213e3735d8738a0872
haniey <h.rastifar@gmail.com>
2022-06-02 08:31:38 -0700

    fix:changing the way use axios fetch data

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,35 +1,36 @@
 import React, { useState, useEffect } from "react";
-import axios from "../api/httpRequestApi";
+import axios from "../api/HttpRequestApi";
 import { toast } from 'react-toastify';
+import api from '../api/api'
 
 const useAxios = (url, requestConfig = {}) => {
-  const [response, setResponse] = useState([]);
-  const [headers, setHeaders] = useState([]);
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
-  // const [controller, setController] = useState();
+  // const [response, setResponse] = useState([]);
+  // const [headers, setHeaders] = useState([]);
+  // const [error, setError] = useState("");
+  // const [loading, setLoading] = useState(false);
+  // // const [controller, setController] = useState();
 
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get(url, {
-        ...requestConfig,
-      })
-      .then((res) => {
-        setResponse(res.data);
-        setHeaders(res.headers);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setError(error);
-        setLoading(false);
-        if (error.response?.status === 401) {
-          toast.error("شما دسترسی لازم را ندارید");
-        } else {
-          toast.error("خطایی روی داده است");
-        }
-      });
-  }, [url]);
+  // useEffect(() => {
+  //   setLoading(true);
+  //   axios
+  //     .get(url, {
+  //       ...requestConfig,
+  //     })
+  //     .then((res) => {
+  //       setResponse(res.data);
+  //       setHeaders(res.headers);
+  //       setLoading(false);
+  //     })
+  //     .catch((error) => {
+  //       setError(error);
+  //       setLoading(false);
+  //       if (error.response?.status === 401) {
+  //         toast.error("شما دسترسی لازم را ندارید");
+  //       } else {
+  //         toast.error("خطایی روی داده است");
+  //       }
+  //     });
+  // }, [url]);
 
   // const axiosFetch = async (configObj) => {
   //   const { url, requestConfig = {} } = configObj;
@@ -55,7 +56,27 @@ const useAxios = (url, requestConfig = {}) => {
   //         console.log(controller);
   //         return (()=>controller.abort())
   // },[controller])
-  return { products: response, error, loading,headers };
+ // return { products: response, error, loading,headers };
+ const [data, setData] = useState(null);
+ const [loading, setLoading] = useState(true);
+ const [error, setError] = useState(null);
+
+ useEffect(() => {
+   (async () => {
+     try {
+       setLoading(true);
+       const response = await api.get(url);
+     
+       setData(response);
+     } catch (error) {
+       setError(error);
+     } finally {
+       setLoading(false);
+     }
+   })();
+ }, [url]);
+
+ return { data, loading, error };
 };
 
 export default useAxios;
