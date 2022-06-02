d1d6c7d8571306a0e78082d5c517948a38f03b41
haniey <h.rastifar@gmail.com>
2022-06-02 08:42:20 -0700

    feat:adding toastify container into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import MyRoutes from "./routes/MyRoutes";
 import {store} from "./redux/store";
 import { Provider } from "react-redux";
+import {  ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div>
+         <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       <Provider store={store}>
         <MyRoutes />
       </Provider>
