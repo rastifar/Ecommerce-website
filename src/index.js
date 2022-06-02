3876f6f3fc4c88438f35e8811731af52629bfc7f
haniey <h.rastifar@gmail.com>
2022-06-02 08:43:47 -0700

    feat:wrapping rtl around components

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "@mui/material";
-import { theme } from "./styles/styles"
+import { theme } from "./assets/material_ui/styles";
+ import RTL from './assets/material_ui/RTL';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<ThemeProvider theme={theme}><App /></ThemeProvider>);
+root.render(<ThemeProvider theme={theme}><RTL><App/></RTL></ThemeProvider>);
