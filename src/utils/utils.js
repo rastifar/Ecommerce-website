12699ae283afc265768982599a3422a972bab5fe
haniey <h.rastifar@gmail.com>
2022-06-02 08:40:24 -0700

    feat:adding some function in utils file inorder to check the existance of a product in the cart slice

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,36 @@
-export const convertTimeStamToDate = (timestamp) => {
+ const convertTimeStamToDate = (timestamp) => {
   const date = new Date(timestamp).toLocaleDateString("fa-IR");
   return date;
 };
 
-export const splitArrayOfImage = (arrayOfImage) => {
-  console.log(arrayOfImage);
-  const array = arrayOfImage.split(',')
-  console.log(array);
-  return array;
+function toFarsiNumber(n) {
+  const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+
+  return n
+      .toString()
+      .replace(/\d/g, x => farsiDigits[x]);
 }
+ const numberDivider = (number) => {
+  if (number) {    
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+  return ""
+}
+
+ const isInCart = (state,id) => {
+   const result = !!state.cartItems.find(item => item.id === id) 
+  return result
+}
+
+ const quantityCount = (state, id) => {
+   const itemIndex = state.cartItems.findIndex(item => item.id === id)
+ 
+  if (itemIndex === -1) {
+    return false
+  } else {   
+    return state.cartItems[itemIndex].quantity
+  }
+}
+
+
+export {convertTimeStamToDate,numberDivider,isInCart,quantityCount ,toFarsiNumber};
