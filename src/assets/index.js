991c8d5e9805638ccdc7f4b05bd8b0a41e3a1a6a
haniey <h.rastifar@gmail.com>
2022-06-03 04:19:44 -0700

    feat:puting all image address in index.js file in assets folder in order to have an easer navigation

---
+++ src/assets/index.js
@@ -0,0 +1,33 @@
+import HomeSlide1 from "./images/homePage.jpg";
+import HomeSlide2 from "./images/frozenHero.jpeg";
+import HomeSlide3 from "./images/smoothyHero.png";
+import Mes1 from "./images/h1s.png";
+import Mes2 from "./images/h2s.png";
+import Mes3 from "./images/h3s.png";
+import Mes4 from "./images/h4s.png";
+import LogoMain from "./images/logo22.png";
+import LogoResponsive from "./images/logo1.png";
+import LogoLogin from "./images/pomegranate1.png";
+import Preview from "./images/preview.jpg";
+import Footer from "./images/ppnm.png";
+import Success from "./images/success.png";
+import Failed from "./images/failure.jpg";
+
+const Images = {
+  HomeSlide1,
+  HomeSlide2,
+  HomeSlide3,
+  Mes1,
+  Mes2,
+  Mes3,
+  Mes4,
+  LogoMain,
+  LogoResponsive,
+  LogoLogin,
+  Preview,
+  Footer,
+  Success,
+  Failed,
+};
+
+export default Images;
