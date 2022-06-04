0d4c1f2b144c987f917ac553c570ba310f56f7f3
haniey <h.rastifar@gmail.com>
2022-06-04 00:41:45 -0700

    fix:changing the grid layout of product group component in order to have items in exact center

--- src/pages/productGroups/ProductGroups.js
+++ src/pages/productGroups/ProductGroups.js
@@ -1,11 +1,18 @@
-import React, { useEffect, useState,useMemo } from "react";
-import { useParams,useLocation } from "react-router-dom";
-import { Grid, Box, ListItemText, List,Pagination,CircularProgress} from "@mui/material";
+import React, { useEffect, useState, useMemo } from "react";
+import { useParams, useLocation } from "react-router-dom";
+import {
+  Grid,
+  Box,
+  ListItemText,
+  List,
+  Pagination,
+  CircularProgress,
+  CssBaseline,
+} from "@mui/material";
 
 import axios from "axios";
 import { caterories } from "../../constants/formsConst";
-import useFetch from '../../hooks/useFetch'
-
+import useFetch from "../../hooks/useFetch";
 
 import ProductCards from "../../components/ProductCards";
 import MyLink from "../../components/MyLink";
@@ -14,93 +21,107 @@ const ProductGroups = () => {
   const [activePage, setActivePage] = useState(1);
   const { categoryNum } = useParams();
 
-  const { data, loading, error } = useFetch(
+  const { data, loading, error, headers } = useFetch(
     `http://localhost:3002/products?category=${categoryNum}&_page=${activePage}&_limit=${limit}}&_sort=asc`
   );
-  //const [data, setData] = useState([]);
-  // const { products, error, loading, axiosFetch } = useFetch();
-  // useEffect(() => {
-  //   axios
-  //     .get(`http://localhost:3002/products?category=${categoryNum}`)
-  //     .then((res) => setData(res.data));
-  // }, [categoryNum]);
 
   return (
-    <Grid
-      container
-      sx={{ my: 4 }}
-      direction="column"
+    <Box
+      display="flex"
+      flexDirection="column"
       alignItems="center"
       justifyContent="center"
-      
+      sx={{ mt: "2rem" }}
     >
-      {/* <Grid item>
+      {loading ? (
         <Box>
-          {caterories.map((item) => (
-            <MyLink to={`/productgroup/${item.value}`} key={item.value}>
-              <Box
-                component="span"
-                sx={{ mx: 2, p: 2, border: "1px dashed grey",my:3,display:'inline-block' }}
-              >
-                {item.label}
-              </Box>
-            </MyLink>
-          ))}
+          <CircularProgress />
         </Box>
-      </Grid> */}
-      {/* <Grid item>
-        <h1> محصولات</h1>
-      </Grid> */}
-       {loading ? (
-              <Box
-                sx={{
-                  position: "absolute",
-                  background: "#fafafa",
-                  width: "100%",
-                  height: "100%",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center"
-                }}
-              >
-                <CircularProgress />
-              </Box>
-            ) : (
-              <>
-               <Grid
-        container
-              direction="row"
-           
-        sx={{ textAlign: "center" }}
-      // spacing={{ xs: 2, md: 3 }}
-        // columns={{ xs: 4, sm: 8, md: 12 }}
-      >
-        {data?.data.map((item) => (
-          <Grid item key={item.name} xs={12} sm={6} md={4}  >
-          
-              <ProductCards productData={item}
-                // image={item.image}
-                // name={item.name}
-                // price={item.price}
-              />
-           
+      ) : (
+        <Box>
+          <CssBaseline />
+          <Grid container>
+            {data?.map((item) => (
+              <Grid item key={item.name} xs={12} sm={6} md={4} align="center">
+                <ProductCards productData={item} />
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
-              </>
-            )}
-   <Box sx={{mt:15}}>
-      <Pagination    variant="outlined"
-        defaultPage={1}
-        page={activePage}
-        count={Math.ceil(Number(data?.headers["x-total-count"]) / Number(limit))}
-        onChange={(_, page) => {
-          console.log("page:", page);
-          setActivePage(page);
-          }} />
         </Box>
-    </Grid>
+      )}
+      {headers && (
+        <Box sx={{ my: 5 }}>
+          <Pagination
+            variant="outlined"
+            defaultPage={1}
+            page={activePage}
+            count={Math.ceil(Number(headers["x-total-count"]) / Number(limit))}
+            onChange={(_, page) => {
+              console.log("page:", page);
+              setActivePage(page);
+            }}
+          />
+        </Box>
+      )}
+    </Box>
   );
 };
 
 export default ProductGroups;
+
+//     {loading ? (
+//       <Box
+//         sx={{
+//           position: "absolute",
+//           background: "#fafafa",
+//           width: "100%",
+//           height: "100%",
+//           display: "flex",
+//           alignItems: "center",
+//           justifyContent: "center",
+//         }}
+//       >
+//         <CircularProgress />
+//       </Box>
+//     ) : (
+//       <>
+//         <Box
+//   display="flex"
+//   flexDirection="column"
+//   alignItems="center"
+//   justifyContent="center"
+// >
+//         <Grid
+//           container
+//           direction="row"
+//           sx={{ textAlign: "center" }}
+//           // spacing={{ xs: 2, md: 3 }}
+//           // columns={{ xs: 4, sm: 8, md: 12 }}
+//         >
+//           {data?.map((item) => (
+//             <Grid item key={item.name} xs={12} sm={6} md={4}>
+//               <ProductCards
+//                 productData={item}
+//                 // image={item.image}
+//                 // name={item.name}
+//                 // price={item.price}
+//               />
+//             </Grid>
+//           ))}
+//           </Grid>
+//           </Box>
+//       </>
+//     )}
+//     <Box sx={{ mt: 15 }}>
+//       <Pagination
+//         variant="outlined"
+//         defaultPage={1}
+//         page={activePage}
+//         count={Math.ceil(Number(headers["x-total-count"]) / Number(limit))}
+//         onChange={(_, page) => {
+//           console.log("page:", page);
+//           setActivePage(page);
+//         }}
+//       />
+//     </Box>
+//   </Grid>
