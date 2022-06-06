9fd96f71d28b7577cf06cafbe9c66f050020dd38
haniey <h.rastifar@gmail.com>
2022-06-06 09:55:15 -0700

    fix:moving my custom carousel component from home directory to shared component directory

--- src/components/CustomeCarousel.js
+++ src/components/CustomeCarousel.js
@@ -1,24 +1,99 @@
-import { Box } from "@mui/material";
-import React, { Component } from "react";
-import Slider from "react-slick";
-import ProductCards from "./ProductCards";
+import React from "react";
+import { Container, Grid, Slider, Typography, Box } from "@mui/material";
 
-const CustomeCarousel = ({ settings, slidesData, isImg }) => {
+
+//Swiper
+// import Swiper core and required modules
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+import "swiper/css/autoplay";
+
+const Carousel = ({
+  Slides,
+  width,
+  height,
+  padding,
+  isImg,
+  moludes,
+  slidesPerView,
+  slidesPerGroup
+}) => {
   return (
-    <Box>
-      <Slider {...settings}>
-        {slidesData?.map((slide, index) => (
-          <div key={index}>
-            {isImg ? (
-              <img src={slide} style={{ width: "100vw", borderRadius: "8px" }} />
-            ) : (
-              <ProductCards productData={slide} />
-            )}
-          </div>
+    <Box sx={{width: {width} ,height:{height}}}>
+      <Swiper
+        dir="rtl"
+        effect="fade"
+        modules={moludes}
+        slidesPerView={slidesPerView}
+        slidesPerGroup={slidesPerGroup}
+        navigation={!isImg ? true : false}
+        autoplay={isImg ? { delay: 3000, disableOnInteraction: false } : false}
+        pagination={{ clickable: true }}
+        style={{
+           width: "100%", height: "100%",
+          "--swiper-navigation-size": "30px",
+          "--swiper-navigation-color": "#d3a98c",
+          "--swiper-pagination-color": "#d3a98c",
+        
+        }}
+        grabCursor={true}
+        spaceBetween={30}
+        centeredSlides={true}
+        zoom={false}
+        //slidesPerGroup={1}
+        loop={true}
+        loopFillGroupWithBlank={false}
+        
+      >
+        {Slides?.map((item, index) => (
+          <SwiperSlide
+            key={index}
+          
+            modules={moludes}
+            sx={{
+              paddingTop: "100%",
+              overflow: "hidden",
+              position: "relative",
+              
+            }}
+            // breakpoints={{
+            //   // when window width is >= 640px
+            //   640: {
+            //     width: 640,
+            //     height: 680,
+            //     backgroun:'yellow',
+            //     slidesPerView: 1,
+            //   },
+            //   // when window width is >= 768px
+            //   768: {
+            //     width: 768,
+            //     backgroun:'green',
+            //     slidesPerView: 1,
+            //   },
+            // }}
+          >
+            <div>
+            
+                <img
+                  src={item}
+                  style={{ width: "100%", borderRadius: "8px",objectFit:'cover' }}
+
+                />
+               
+             
+            </div>
+          </SwiperSlide>
         ))}
-      </Slider>
-    </Box>
+      </Swiper>
+      </Box>
   );
 };
 
-export default CustomeCarousel;
+export default Carousel;
+
+// { delay: 100000, disableOnInteraction: false }
