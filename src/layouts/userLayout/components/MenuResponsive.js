f634783bf4cdb99e0c01f1d12ffde261178a72a6
haniey <h.rastifar@gmail.com>
2022-06-06 09:36:43 -0700

    feat:creating a menuResponsive component to use menu in responsive mode'

---
+++ src/layouts/userLayout/components/MenuResponsive.js
@@ -0,0 +1,65 @@
+import * as React from "react";
+import {
+  Button,
+  Divider,
+  MenuItem,
+  Menu,
+  Box,
+  Typography,
+} from "@mui/material";
+
+import { products } from "../../../constants/sidebarConst";
+import MyList from "../../../components/MyList";
+import MyLink from "../../../components/MyLink";
+
+export default function MenuResponsive({ icon, menuLink, submenuTitle }) {
+  console.log('menuLink',menuLink);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      {/* <Button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+        variant="outlined"
+        sx={{ mt: 1, mx: 1 }}
+      > */}
+     
+        <Box onClick={handleClick} sx={{ border: "1px solid lightgray" }}>
+          <Typography textAlign={"center"}>{icon}</Typography>
+        </Box>
+      
+      {/* </Button> */}
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        {submenuTitle.map((product, index) => (
+          <MenuItem onClick={handleClose}>
+            {/* <MyList
+              title={product.category}
+              submenuTitle={product.subcategory}
+            />
+            <Divider /> */}
+            <MyLink to={product.link}>{product.title}</MyLink>
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
+  );
+}
