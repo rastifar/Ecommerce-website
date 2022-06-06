a6c2731881cb4a88b9f0a112d8ab82526e82b092
haniey <h.rastifar@gmail.com>
2022-06-06 10:26:45 -0700

    feat:adding subcategori constants to formconst file in constant folder

--- src/constants/formsConst.js
+++ src/constants/formsConst.js
@@ -1,14 +1,26 @@
 export const caterories = [
     {
         label: 'میوه و سبزی تازه',
-        value:'1'
+        value: '1',
+        subcategory: [
+            { label: 'میوه', value: '1' },
+            {label:'سبزی',value:'2'}
+        ]
     },
     {
         label: 'میوه و سبزی منجمد',
-        value:'2'
+        value:'2',
+        subcategory: [
+            { label: 'میوه', value: '3' },
+            {label:'سبزی',value:'4'}
+        ]
     },
     {
         label: 'نوشیدنی ها',
-        value:'3'
+        value:'3',
+        subcategory: [
+            { label: 'میوه', value: '5' },
+            {label:'سبزی',value:'6'}
+        ]
     }
 ]
