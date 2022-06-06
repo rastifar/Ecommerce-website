9d30e83db026aa8133d6f1c75135a7ec8f82a3a2
haniey <h.rastifar@gmail.com>
2022-06-06 09:32:40 -0700

    feat:creating a cudtom select component to use in different places

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,27 +1,38 @@
-import React from "react";
-import { 
-  MenuItem,
-  InputLabel,
-  Box, 
-  Select,
-    FormControl,
-    
-} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { MenuItem, InputLabel, Box, Select, FormControl } from "@mui/material";
+import { useLocation, useSearchParams } from "react-router-dom";
 
-const Selects = ({ label, items, value, setValue }) => {
+const Selects = ({ label, items }) => {
+  console.log(items);
+  const location = useLocation();
+  const currentLocation = location.pathname.split("/").pop();
+  useEffect(() => {
+    setValue('')
+  }, [currentLocation]);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [value, setValue] = useState('');
+
   return (
-    <Box sx={{ minWidth: 80,mt:1 }}>
-      <FormControl fullWidth sx={{mt:.2}}>
+    <Box sx={{ minWidth: 80, mt: 1 }}>
+      <FormControl fullWidth sx={{ mt: 0.2 }}>
         <InputLabel id="demo-simple-select-label">{label}</InputLabel>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
           label={label}
           value={value}
-                  onChange={(e) =>  setValue(e.target.value)}
+          onChange={(e) => {
+            {
+              setValue(e.target.value);
+              setSearchParams(e.target.value);
+            }
+          }}
         >
           {items?.map((item, index) => (
-              <MenuItem key={index} value={index}>{item}</MenuItem>
+            <MenuItem key={index} value={item.link}>
+              {item.title}
+            </MenuItem>
           ))}
         </Select>
       </FormControl>
