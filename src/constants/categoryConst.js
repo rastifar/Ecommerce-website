9dc9dbe89d3e5b24b45e7604c24b9754b779acd6
haniey <h.rastifar@gmail.com>
2022-06-06 18:26:24 -0700

    feat:adding an array of values for submenu in order to render its name instead of its number

--- src/constants/categoryConst.js
+++ src/constants/categoryConst.js
@@ -1,5 +1,6 @@
-export const category = ["میوه و سبزی تازه", "میوه و سبزی منجمد", "نوشیدنی ها"];
+ const category = ["میوه و سبزی تازه", "میوه و سبزی منجمد", "نوشیدنی ها"];
+ const subCategory = ["میوه", "سبزی"];
+ const breadcrump = ["میوه و سبزی تازه", "میوه و سبزی منجمد", "نوشیدنی ها"];
 
-export const breadcrump = ["میوه و سبزی تازه", "میوه و سبزی منجمد", "نوشیدنی ها"];
 
-
+export{category,subCategory,breadcrump}
