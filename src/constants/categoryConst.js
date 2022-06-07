615cb3b98701d2ccb66ac6022692860d0c25032d
haniey <h.rastifar@gmail.com>
2022-06-07 01:04:59 -0700

    fix:removing unnecessary array from categoryConst

--- src/constants/categoryConst.js
+++ src/constants/categoryConst.js
@@ -1,6 +1,6 @@
  const Category = ["میوه و سبزی تازه", "میوه و سبزی منجمد", "نوشیدنی ها"];
  const subCategory = ["میوه", "سبزی"];
- const breadcrump = ["میوه و سبزی تازه", "میوه و سبزی منجمد", "نوشیدنی ها"];
 
 
-export{Category,subCategory,breadcrump}
+
+export{Category,subCategory}
