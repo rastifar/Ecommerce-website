5a812318ec619ca326ea5290bdf4c2f5b94c3d6d
haniey <h.rastifar@gmail.com>
2022-06-06 21:44:32 -0700

    fix:adding key inside map to aviod getting warning

--- src/layouts/userLayout/components/Sidebar.js
+++ src/layouts/userLayout/components/Sidebar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 //-----------------Material
 import {
-  Paper,  
+  Paper,
   Box,
   Typography,
   Divider,
@@ -55,7 +55,7 @@ const Sidebar = () => {
                   <Box
                     key={index}
                     sx={{
-                      flexBasis: "30%",                      
+                      flexBasis: "30%",
                       mr: "auto",
                       ml: "auto",
                     }}
@@ -66,8 +66,8 @@ const Sidebar = () => {
                       icon={product.icon}
                       menuLink={product.link}
                       submenuTitle={product.subcategory}
-                        />
-                        {/* </Box>
+                    />
+                    {/* </Box>
                       </MyLink> */}
                   </Box>
                 ))}
@@ -80,16 +80,14 @@ const Sidebar = () => {
                 <Divider />
 
                 {products.map((product, index) => (
-                  <>
-                    {/* <MyLink to={product.link} key={index}> */}
-                      <MyList
-                        title={product.category}
-                        menuLink={product.link}
-                        submenuTitle={product.subcategory}
-                      />
-                      <Divider />
-                    {/* </MyLink> */}
-                  </>
+                  <div key={index}>
+                    <MyList
+                      title={product.category}
+                      menuLink={product.link}
+                      submenuTitle={product.subcategory}
+                    />
+                    <Divider />
+                  </div>
                 ))}
               </>
             )}
@@ -107,136 +105,3 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
-
-// import React, { useState } from "react";
-// //-----------------Material
-// import {
-//   Paper,
-//   List,
-//   ListItem,
-//   Box,
-//   Typography,
-//   Divider,
-//   useTheme,
-//   useMediaQuery,
-// } from "@mui/material";
-// import { styled } from "@mui/material/styles";
-// //-----------------Component
-// import MyLink from "../../../components/MyLink";
-// import Select from "../../../components/Select";
-
-// const MyPaper = styled(Paper)(({ theme }) => ({
-//   display: "none",
-//   color: "text.secondary",
-//   marginTop: "2rem",
-//   paddingLeft: "10px",
-//   marginLeft: "3px",
-//   minHeight: "100vh",
-//   overflow: "auto",
-//   padding: 8,
-//   borderRadius: 4,
-//   width: "25vw",
-//   transition: "transform 1s",
-//   [theme.breakpoints.up("sm")]: {
-//     display: 'block',
-//     width:'98vw'
-//   },
-//   [theme.breakpoints.down("md")]: {
-//     display: 'block',
-//     width:'98vw'
-//   },
-
-//   // [theme.breakpoints.up("sm")]: {
-//   //   backgroundColor: "pink",
-//   // color:'pink',
-//   //   width:'98vw'
-//   // },
-//   // [theme.breakpoints.down("md")]: {
-//   //   backgroundColor: "yellow",
-
-//   //   color: "black",
-//   // },
-//   // [theme.breakpoints.up("lg")]: {
-//   //   backgroundColor: "green",
-//   //   color: "white",
-//   // },
-// }));
-
-// const products = [
-//   {
-//     category: "میوه و سبزی تازه",
-//     link: "/productgroup/1",
-//   },
-//   {
-//     category: "میوه و سبزی منجمد",
-//     link: "/productgroup/2",
-//   },
-//   {
-//     category: "نوشیدنی",
-//     link: "/productgroup/3",
-//   },
-// ];
-
-// const Sidebar = ({}) => {
-//   const theme = useTheme();
-//   const largeScreen = useMediaQuery(theme.breakpoints.down("lg"));
-//   const [category, setCategory] = useState("");
-//   const [price, setPrice] = useState("");
-//   const [popularity, setPopularity] = useState("");
-//   return (
-//     <>
-//       <MyPaper square variant="outlined">
-//         <Box
-//           display={"flex"}
-//           flexDirection={largeScreen ? "row" : "column"}
-//           width="100%"
-//         >
-//           <Typography textAlign={"center"} py={2}>
-//             لیست محصولات
-//           </Typography>
-//           <Divider />
-
-//         {largeScreen ? (
-//           <List component="nav">
-//             {products.map((product, index) => (
-//               <MyLink to={product.link} key={index}>
-//                 <ListItem button component={"li"}>
-//                   <Typography py={1}>{product.category}</Typography>
-//                 </ListItem>
-//                 <Divider />
-//               </MyLink>
-//             ))}
-//           </List>
-//         ) : (
-//           <Box>
-//             <Select
-//               label={"دسته بندی محصولات"}
-//               items={["میوه و سبزی تازه", "میوه و سبزی منجمد", "نوشیدنی ها"]}
-//               value={category}
-//               setValue={setCategory}
-//             />
-//           </Box>
-//         )}
-//         <Box>
-//           <Select
-//             label={"فیلتر قیمت"}
-//             items={["به ترتیب صعودی", "به ترتیب نزولی"]}
-//             value={price}
-//             setValue={setPrice}
-//           />
-//         </Box>
-//         <Box>
-//           <Select
-//             label={"فیلتر ویژگی ها"}
-//             items={["محبوب ترین ها", "پرفروش ترین ها"]}
-//             value={popularity}
-//             setValue={setPopularity}
-//           />
-//           </Box>
-//           </Box>
-//       </MyPaper>
-//     </>
-//   );
-// };
-
-// export default Sidebar;
