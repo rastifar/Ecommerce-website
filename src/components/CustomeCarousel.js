cc559ba43f35f3c88722fae036d43a66cc2dd238
haniey <h.rastifar@gmail.com>
2022-06-07 02:33:39 -0700

    fix:removing the Box component from customeCarousel

--- src/components/CustomeCarousel.js
+++ src/components/CustomeCarousel.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Container, Grid, Slider, Typography, Box } from "@mui/material";
 
-
 //Swiper
 // import Swiper core and required modules
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -12,6 +11,7 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
 import "swiper/css/autoplay";
+import { BASE_URL } from "../constants/apiConst";
 
 const Carousel = ({
   Slides,
@@ -21,76 +21,87 @@ const Carousel = ({
   isImg,
   moludes,
   slidesPerView,
-  slidesPerGroup
+  slidesPerGroup,
 }) => {
   return (
-    <Box sx={{width: {width} ,height:{height}}}>
-      <Swiper
-        dir="rtl"
-        effect="fade"
-        modules={moludes}
-        slidesPerView={slidesPerView}
-        slidesPerGroup={slidesPerGroup}
-        navigation={!isImg ? true : false}
-        autoplay={isImg ? { delay: 3000, disableOnInteraction: false } : false}
-        pagination={{ clickable: true }}
-        style={{
-           width: "100%", height: "100%",
-          "--swiper-navigation-size": "30px",
-          "--swiper-navigation-color": "#d3a98c",
-          "--swiper-pagination-color": "#d3a98c",
-        
-        }}
-        grabCursor={true}
-        spaceBetween={30}
-        centeredSlides={true}
-        zoom={false}
-        //slidesPerGroup={1}
-        loop={true}
-        loopFillGroupWithBlank={false}
-        
-      >
-        {Slides?.map((item, index) => (
-          <SwiperSlide
-            key={index}
-          
-            modules={moludes}
-            sx={{
-              paddingTop: "100%",
-              overflow: "hidden",
-              position: "relative",
-              
-            }}
-            // breakpoints={{
-            //   // when window width is >= 640px
-            //   640: {
-            //     width: 640,
-            //     height: 680,
-            //     backgroun:'yellow',
-            //     slidesPerView: 1,
-            //   },
-            //   // when window width is >= 768px
-            //   768: {
-            //     width: 768,
-            //     backgroun:'green',
-            //     slidesPerView: 1,
-            //   },
-            // }}
-          >
-            <div>
-            
-                <img
-                  src={item}
-                  style={{ width: "100%", borderRadius: "8px",objectFit:'cover' }}
-
-                />
-               
-             
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      </Box>
+    // <Box sx={{width:"100%" ,height:"100%"}}>
+    <Swiper
+      dir="rtl"
+      effect="fade"
+      modules={moludes}
+      slidesPerView={slidesPerView}
+      slidesPerGroup={slidesPerGroup}
+      navigation={!isImg ? true : false}
+      autoplay={isImg ? { delay: 3000, disableOnInteraction: false } : false}
+      pagination={{ clickable: true }}
+      style={{
+        width: "100%",
+        height: "100%",
+        "--swiper-navigation-size": "30px",
+        "--swiper-navigation-color": "#d3a98c",
+        "--swiper-navigation-color": "#d3a98c",
+        "--swiper-button-prev":"blue",
+        "--swiper-pagination-color": "#d3a98c",
+     
+      }}
+      grabCursor={true}
+      spaceBetween={30}
+      centeredSlides={true}
+      zoom={false}
+      //slidesPerGroup={1}
+      loop={true}
+      loopFillGroupWithBlank={false}
+    >
+      {Slides?.map((item, index) => (
+        <SwiperSlide
+          key={index}
+          modules={moludes}
+          // sx={{
+          //   paddingTop: "100%",
+          //   overflow: "hidden",
+          //   position: "relative",
+          // }}
+          // breakpoints={{
+          //   // when window width is >= 640px
+          //   640: {
+          //     width: 640,
+          //     height: 680,
+          //     backgroun:'yellow',
+          //     slidesPerView: 1,
+          //   },
+          //   // when window width is >= 768px
+          //   768: {
+          //     width: 768,
+          //     backgroun:'green',
+          //     slidesPerView: 1,
+          //   },
+          // }}
+        >
+          <div>
+            {isImg ? (
+              <img
+                src={item}
+                style={{
+                  width: "100%",
+                  borderRadius: "8px",
+                  objectFit: "contain",
+                }}
+              />
+            ) : (
+              <img
+                src={BASE_URL + "/files/" + item}
+                style={{
+                  width: "100%",
+                  borderRadius: "8px",
+                  objectFit: "cover",
+                }}
+              />
+            )}
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+    // </Box>
   );
 };
 
