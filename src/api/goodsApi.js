ee6bd209efa3b74b9e16c9101edb14b262cc8a65
haniey <h.rastifar@gmail.com>
2022-06-07 21:35:46 -0700

    feat:adding delete post patch method for goods api calls

--- src/api/goodsApi.js
+++ src/api/goodsApi.js
@@ -1,33 +1,57 @@
 import HttpService from "../services/httpService";
 import { PRODUCTS } from "../constants/apiConst";
 import { toast } from "react-toastify";
+import httpService from "../services/httpService";
 
-const getAllProducts = async (sort) => {
+const getAllProducts = async (sortOrder) => {
   try {
-    const response = await HttpService.get(PRODUCTS + sort);   
+    const response = await HttpService.get(PRODUCTS + sortOrder);
     return response.data;
   } catch (error) {
     toast.error("خطایی در بارگذاری اطلاعات محصول رخ داده است");
   }
 };
-
-const patchRatingProductById = async (val, productid) => {
-  HttpService.patch(
-    PRODUCTS + `/${productid}`,
-    { favorite: val },
-    {
-      "Content-Type": "application/json",
+const deleteProductById = async (productId) => {
+  try {
+    const response = await httpService.delete(PRODUCTS + `/${productId}`);
+    if (response.status == 200 || response.status == 201) {
+      toast.success("محصول با موفقیت حذف شده است");
     }
-  )
-    .then((res) => {
-      if (res.status === 200 || res.status === 201) {
-        toast.success("نظر شما با موفقیت ثبت شده است");
-      }
-    })
-    .catch((error) =>
-      toast.error(
-        "مشکلی در ثبت نظر شمابه وجود امده است لطفا مجددا نظر خود را ثبت کنید"
-      )
-    );
+  } catch (error) {
+    toast.error("خطایی در حذف محصول روی داده است لطفادوباره امتحان کنید");
+  }
 };
-export { getAllProducts, patchRatingProductById };
+
+const editProductByid = async (productId, values) => {
+  try {
+    const response = await HttpService.patch(
+      PRODUCTS + `/${productId}`,
+      values,
+      {
+        "Content-Type": "application/json",
+      }
+    );
+    if (response.status == 200 || response.status == 201) {
+      toast.success("اطلاعات با موفقیت به روز رسانی شده است");
+    }
+  } catch (error) {
+    toast.error("عملیات به درستی انجام نشده است");
+  }
+};
+const createProduct = async (values) => {
+  try {
+    const response = await httpService.post(PRODUCTS, values);
+    if (response.status == 200 || response.status == 201) {
+      toast.success("اطلاعات با موفقیت ثبت شده است");
+    }
+  } catch (error) {
+    toast.error("عملیات به درستی انجام نشده است");
+  }
+};
+
+export {
+  getAllProducts,
+  deleteProductById,
+  createProduct,
+  editProductByid,
+};
