8c1f434aae414f81a51242c68d7d61b5e399ea31
haniey <h.rastifar@gmail.com>
2022-06-07 20:47:09 -0700

    fix:applying headers token  to all routes except login

--- src/api/httpRequestApi.js
+++ src/api/httpRequestApi.js
@@ -1,54 +1,85 @@
 import axios from "axios";
 import { BASE_URL } from "../constants/apiConst";
-class HttpService {
-  constructor() {
-    axios.defaults.baseURL = BASE_URL;
 
-    axios.interceptors.request.use(
-      (config) => {
-        // const token = useSelector((state) => state.token);
-        // if (config.url !== LOGIN) {
-        //   config.headers["token"] = `${token}`;
-        // }
-        return config;
-      },
-      (error) => {
-        toast.error(error.response.data);
-        return Promise.reject(error);
-      }
-    );
-
-    axios.interceptors.response.use(
-      (response) => {
-        if (response.statusCode === 401) {
-          toast.error("شما دسترسی لازم برای این کار را ندارید");
-        }
-        return response;
-      },
-      (error) => {
-        return Promise.reject(error);
-      }
-    );
-  }
-  get(url, config) {
-    return axios.get(url, config);
-  }
-
-  post(url, data, config) {
-    return axios.post(url, data, config);
-  }
-
-  put(url, data, config) {
-    return axios.put(url, data, config);
-  }
-
-  patch(url, data, config) {
-    return axios.patch(url, data, config);
-  }
-
-  delete(url, config) {
-    return axios.delete(url, config);
-  }
-}
-
-export default new HttpService();
+export default axios.create({
+    baseURL: BASE_URL,
+    headers: { 'Content-Type': 'application/json' }
+});
+// class HttpService {
+   
+//     constructor() {
+//       axios.defaults.baseURL = BASE_URL;
+//       axios.defaults.timeout = 2000;
+//       axios.interceptors.request.use(
+//         (config) => {
+//           if(config.url==="orders"){
+//             config.headers["token"]=localStorage.getItem("token")
+//           }
+//           //config.headers["X-Total-Count"]=config.lenght
+//           return config;
+//         },
+//         (error) => {
+//           return Promise.reject(error);
+//         }
+//       );
+  
+//       axios.interceptors.response.use(
+//         (response) => {
+//           // if(response.statusCode ===401) {
+//           //   alert ("you have  to set token")
+//           // }
+           
+//           return response;
+//         },
+//         (error) => {
+//           return Promise.reject(error);
+//         }
+//       );
+//     }
+  
+//     get = async(url) => {
+//       try {
+//         const res = await axios.get(url);
+//         return res;
+//       } catch (err) {
+//         return err;
+//       }
+  
+//     };
+  
+//     post = async(url,data) => {
+//       try {
+//         const res = await axios.post(url,data);
+//         return res.data;
+//       } catch (err) {
+//         return err;
+//       }
+  
+//     };
+  
+//     patch = async(url,data) => {
+//       try {
+//         const resp = await axios.patch(url,data);
+//         return resp.data;
+//       } catch (err) {
+//         return err;
+//       }
+//     };
+  
+//     delete = async(url,data) => {
+//       try {
+//         const resp = await axios.delete(url,data);
+//         return resp.data;
+//       } catch (err) {
+//         return err;
+//       }
+//     };
+  
+//     cancel() {
+//       const abortHandler = new AbortController();
+//       abortHandler.abort();
+//     }
+//   }
+  
+//   export default  new HttpService();
+  
