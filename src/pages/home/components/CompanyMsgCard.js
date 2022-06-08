8960f3b7cae8f367cff3c61ce52caa56dd313b6c
haniey <h.rastifar@gmail.com>
2022-06-07 18:30:26 -0700

    fix:removing link tag from companyMsgcard

--- src/pages/home/components/CompanyMsgCard.js
+++ src/pages/home/components/CompanyMsgCard.js
@@ -1,15 +1,15 @@
 import React from "react";
 import MyLink from "../../../components/MyLink";
 
-const CompanyMsgCard = ({ image, title, description = "", link = "" }) => {
+const CompanyMsgCard = ({ image, title, description = ""}) => {
   return (
-    <MyLink to={link ? link : "#"}>
+    // <MyLink to={link ? link : "#"}>
       <div style={{ textAlign: "center" }}>
         <img src={image} />
         <p style={{ color: "#414141", fontSize: "1.2rem" }}>{title}</p>
         <p style={{ color: "#C1C1C1", fontSize: "1rem" }}>{description}</p>
       </div>
-    </MyLink>
+    // </MyLink>
   );
 };
 
