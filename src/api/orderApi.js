48baf33e0c6f807c1b78c7de4bf9204327cac9fd
haniey <h.rastifar@gmail.com>
2022-06-08 00:57:07 -0700

    feat:adding deliverdOred method in orderApi file

--- src/api/orderApi.js
+++ src/api/orderApi.js
@@ -1,14 +1,26 @@
 import HttpService from "../services/httpService";
-import { ORDERS} from "../constants/apiConst";
+import { ORDERS } from "../constants/apiConst";
 import { toast } from "react-toastify";
 
 const getAllOrders = async (sortOrder) => {
-    try {
-      const response = await HttpService.get(ORDERS + sortOrder);
-      return response.data;
-    } catch (error) {
-      toast.error("خطایی در بارگذاری اطلاعات محصول رخ داده است");
-    }
+  try {
+    const response = await HttpService.get(ORDERS + sortOrder);
+    return response.data;
+  } catch (error) {
+    toast.error("خطایی در بارگذاری اطلاعات محصول رخ داده است");
+  }
 };
-  
-export {getAllOrders}
+
+const deliveredOrder = async (orderId) => {
+  try {
+    const response = await HttpService.patch(ORDERS + `/${orderId}`, {
+      orderStatus: 1,
+      deliverdAt: +new Date(),
+    });
+    return response;
+  } catch (error) {
+    toast.error("خطایی در انجام عملیات رخ داده است");
+  }
+};
+
+export { getAllOrders, deliveredOrder };
